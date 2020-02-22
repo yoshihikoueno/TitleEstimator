a0c92293c901fc4b3267ee31ccc3d19f3359dc5f
Yoshihiko Ueno <windows7home@hotmail.co.jp>
2020-02-22 20:04:23 +0900

    added fundamental files

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+Sorts title candidates for a given document
+'''
+
+# built-in
+import os
+import argparse
+import pdb
+import json
+import pickle
+
+# external
+import pandas as pd
+import tensorflow as tf
+import tensorflow.keras as keras
+
+# customs
