da7d7ab380a8ff1bb7c47db85760fe1307e481b8
Yoshihiko Ueno <windows7home@hotmail.co.jp>
2020-02-23 16:03:10 +0900

    added to_tagged_docs

--- utils.py
+++ utils.py
@@ -6,12 +6,17 @@ import os
 import pdb
 import json
 import pickle
+from multiprocessing import cpu_count
 
 # external
 import MeCab
 import tensorflow as tf
 import pandas as pd
 import jaconv
+from gensim.models.doc2vec import Doc2Vec
+from gensim.models.doc2vec import TaggedDocument
+import dask.dataframe as dd
+from dask.diagnostics import ProgressBar
 
 
 def split2words(sentence):
@@ -45,3 +50,31 @@ def normalize_string(string):
     '''
     normalized = jaconv.normalize(string)
     return normalized
+
+
+def to_tagged_docs(docs):
+    '''
+    convert documents to genism tagged docs
+
+    Args:
+        docs (DataFrame): doc id and list of words in a doc
+    '''
+    with ProgressBar():
+        tagged_docs = dd.from_pandas(docs, npartitions=cpu_count() * 4)\
+            .apply(to_tagged_doc, axis=1)\
+            .compute()
+    return tagged_docs
+
+
+def to_tagged_doc(doc):
+    '''
+    convert a single doc to genism TaggedDocument
+
+    Args:
+        doc (Series): should contain 'id', 'content'
+
+    Returns:
+        TaggedDocument
+    '''
+    tagged = TaggedDocument(words=doc.content, tags=[doc.id])
+    return tagged
